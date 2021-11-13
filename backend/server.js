bbcda35c63938809a417006b94811e0866ab1e96
JannatN <jannatfaisalnatsheh@gmail.com>
2021-11-13 22:05:44 +0200

    Connect to mongodb

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const cors = require('cors')
 
-require('dotenv').config();
+require("dotenv").config({ path: "./config.env" });
 
 const app = express();
 const port = process.env.PORT || 5000;
@@ -9,6 +9,19 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
+// get driver connection
+const dbo = require("./db/conn");
+
+
+app.get('/', (req, res) => {
+  res.send({ express: 'YOUR EXPRESS BACKEND IS CONNECTED TO REACT' }); 
+}); 
+
 app.listen(port, () => {
+    // perform a database connection when server starts
+    dbo.connectToServer(function (err) {
+      if (err) console.error(err);
+   
+    });
     console.log(`Server is running on port: ${port}`);
-});
+  });
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,38 @@
+import React, { Component } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        WELCOME IN POS SYSTEM
-      </header>
-    </div>
-  );
+class App extends Component {
+state = {
+    data: null
+  };
+
+  componentDidMount() {
+      // Call our fetch function below once the component mounts
+    this.callBackendAPI()
+      .then(res => this.setState({ data: res.express }))
+      .catch(err => console.log(err));
+  }
+    // Fetches our GET route from the Express server. (Note the route we are fetching matches the GET route from server.js
+  callBackendAPI = async () => {
+    const response = await fetch('/');
+    const body = await response.json();
+
+    if (response.status !== 200) {
+      throw Error(body.message) 
+    }
+    return body;
+  };
+
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          WELCOME TO POS SYSTEM
+        </header>
+        <p className="App-intro">{this.state.data}</p>
+      </div>
+    );
+  }
 }
 
 export default App;
