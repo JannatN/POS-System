0eb69ed3b8e39081f51b6ce8c93151cdcb842fba
JannatN <jannatfaisalnatsheh@gmail.com>
2021-12-04 15:49:14 +0200

    sidebar

--- frontend/src/components/sidebar.js
+++ frontend/src/components/sidebar.js
@@ -12,32 +12,50 @@ class Sidebar extends Component {
 
     render() {
         return (
-            <div className="navbar">
-                <div className="title">
-                    <img src={Logo} alt='shop logo' width="45px" />
-                    DiZyni Store</div>
+            <div className="sidebar">
+                <div className="sidebar__title">
+                    <div className="sidebar__img">
+                    <i className="fa fa-home"> <img src={Logo} alt="dashboard_icon" width="50px" /></i>
+                        <h1> DiZyni Store</h1>
+                    </div>
+                    <i
+                        onclick="closeSidebar()"
+                        class="fa fa-times"
+                        id="sidebarIcon"
+                        aria-hidden="true"
+                    ></i>
+                </div>
 
-                <div className="li"> <img src={Dash} alt="dashboard_icon" />
-                    <p>Dashboard</p>
-                </div>
-                <div className="li"> <img src={Stock} alt="stock" />
-                    <p>Stock</p>
-                </div>
-                <div className="li"> <img src={Financial} alt="finan" />
-                    <p>Financial Transactions</p>
-                </div>
-                <div className="li"> <img src={Report} alt="reports" />
-                    <p>Reports</p>
-                </div>
-                <div className="li"> <img src={Cashiers} alt="cashiers" />
-                    <p>Cashiers</p>
-                </div>
-                <div className="li"> <img src={Logout} alt="logout" />
-                    <p>Logout</p>
+                <div className="sidebar__menu">
+                    <div className="sidebar__link active_menu_link">
+                        <i className="fa fa-home"> <img src={Dash} alt="dashboard_icon" width="20px" /></i>
+                        <a href="#">Dashboard</a>
+                    </div>
+                    <div className="sidebar__link">
+                        <i className="fa fa-user-secret" aria-hidden="true" > <img src={Stock} alt="stock" width="20px" /></i>
+                        <a href="#"> Stock</a>
+                    </div>
+                    <div className="sidebar__link">
+                        <i className="fa fa-building-o"> <img src={Financial} alt="finan" width="20px" /></i>
+                        <a href="#">Financial Transactions</a>
+                    </div>
+                    <div className="sidebar__link">
+                        <i className="fa fa-wrench"><img src={Report} alt="reports" width="20px" /></i>
+                        <a href="#">Reports</a>
+                    </div>
+
+                    <div className="sidebar__link">
+                        <i className="fa fa-handshake-o"><img src={Cashiers} alt="cashiers" width="20px" /></i>
+                        <a href="#">Cashiers</a>
+                    </div>
+                    <div className="sidebar__link">
+                        <i className="fa fa-question"><img src={Logout} alt="logout" width="20px" /></i>
+                        <a href="#">Logout</a>
+                    </div>
                 </div>
             </div>
-      );
-  }
+        );
+    }
 }
 
 export default Sidebar;
